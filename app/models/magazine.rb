1e5fd1103e45ecf1e3872dba356b7d25c9317e55
Yosef <shmuli5770@gmail.com>
2021-05-25 11:57:36 -0400

    met deliverables

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class Magazine < ActiveRecord::Base
-  
+    has_many :subscriptions
+    has_many :readers, through: :subscriptions
+
+    def email_list
+        email_array = readers.map { |reader_instance| reader_instance.email }
+        email_array.join(';')
+    end
+
+    def self.most_popular
+        all.sort {|magazine_a, magazine_b| magazine_b.readers.length <=> magazine_a.readers.length}[0]
+    end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,3 +1,17 @@
+require 'pry'
 class Reader < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :magazines, through: :subscriptions
+
+  def subscribe(magazine, price)
+    Subscription.create(reader_id: id, magazine_id: magazine.id, price: price)
+  end
+
+  def total_subcription_price
+    subscriptions.inject(0) { |sum, i|  sum + i.price }
+  end
+
+  def cancel_subscription(magazine)
+    subscriptions.find_by(magazine_id: magazine.id).destroy
+  end
 end
